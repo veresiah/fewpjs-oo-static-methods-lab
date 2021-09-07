5fde22e0a76f2727ac1b958c8ab7f47217512a73
Veresia Hiralall <hiralallveresia@yahoo.com>
2021-09-07 14:42:24 -0400

    commit tree fb6f75c71e8b8138a87fd7388a6842c95b644298

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 class Formatter {
   //add static methods here
+  static capitialize(string) {
+    return string.charAt( 0 ).toUpperCase() + string.slice( 1 )
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9 '-]/g, '')
+  }
+
+  static titleize(string) {
+    let exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let result = [];
+    let sentence = string.split(' ')
+    for (let i = 0; i < sentence.length; i++) {
+      if (i == 0) {
+        result.push(this.capitialize(sentence[i]))
+      } else {
+        if (exceptions.includes(sentence[i])) {
+          result.push(sentence[i])
+        } else {
+          result.push(this.capitialize(sentence[i]))
+        }
+      }
+    }
+    return result.join(' ');
+  }
 }
